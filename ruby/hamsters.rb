3fae427375f4212d765b3857b19d151c21a2624a
Nick Gorajski <nickgorajski@gmail.com>
2016-04-12 18:59:10 -0500

    Adds prompts and some validation to hamsters.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,30 @@
+puts "What is the hamster's name?"
+hamster_name=gets.chomp
+
+
+puts "What is the hamster's volume level (1-10)?"
+volume=gets.chomp.to_i
+until volume >= 1 && volume <= 10 
+puts "Invalid input, please enter a number between 1 and 10."
+volume=gets.chomp.to_i
+end
+
+puts "What is the hamster's fur color?"
+fur_color=gets.chomp
+
+puts "Is the hamster a good candidate for adoption (y/n)?"
+candidate=gets.chomp
+until candidate.capitalize == "Y" || candidate.capitalize == "N"
+puts "Invalid input, please enter either 'y' or 'n'."
+candidate=gets.chomp
+end
+
+#if candidate.capitalize=="Y"
+
+puts "What is the hamster's estimated age?"
+age=gets.chomp.to_f
+p age
+
+p nil
+
+
