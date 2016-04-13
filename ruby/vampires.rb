c034cea66bb012b66a1e421ea8768106267eb05b
Nick Gorajski <nickgorajski@gmail.com>
2016-04-12 21:59:22 -0500

    Adds data collection questions to vampires.rb.

---
+++ ruby/vampires.rb
@@ -0,0 +1,7 @@
+puts "What is your name?"
+
+puts "How old are you?"
+
+puts "Our company cafeteria serves garlic bread.  Should we order some for you?"
+
+puts "Would you like to enroll in the company's health insurance?"
