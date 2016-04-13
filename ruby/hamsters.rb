1769a76bdf1671ff38ec79ed9b898a4026fe8a9c
Nick Gorajski <nickgorajski@gmail.com>
2016-04-12 19:40:27 -0500

    Adds output code to hamsters.rb.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,3 +1,5 @@
+#Gather Input--------------------------------------------------------
+
 puts "What is the hamster's name?"
 hamster_name=gets.chomp
 
@@ -13,10 +15,7 @@ fur_color=gets.chomp
 
 puts "Is the hamster a good candidate for adoption (y/n)?"
 
-
 candidate=gets.chomp
-
-
 until candidate.capitalize == "Y" || candidate.capitalize == "N"
 puts "Invalid input, please enter either 'y' or 'n'."
 candidate=gets.chomp
@@ -28,10 +27,6 @@ else
 	candidate = false
 end
 
-if candidate
-	puts "Success!"
-end
-
 puts "What is the hamster's estimated age?"
 age=gets.chomp
 if age.length == 0
@@ -40,5 +35,20 @@ else
     age = age.to_f
 end
 
+#Output Data---------------------------------------------------
 
+puts "This hamster's name is #{hamster_name}."
+puts "#{hamster_name}'s volume level is #{volume}."
+puts "This hamster's fur is colored #{fur_color}."
 
+if candidate
+	puts "This hamster is a good candidate for adoption!"
+else
+	puts "This hamster is not a good candidate for adoption."
+end
+
+if age == nil
+	puts "The hamster's age is unknown."
+else
+	puts "This hamster's age is #{age}."
+end
