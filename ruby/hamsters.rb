5828c838b71f7f4df7cb9142cbd8e430769c0b55
Nick Gorajski <nickgorajski@gmail.com>
2016-04-12 19:27:57 -0500

    Adds data validation to age, candidacy and volume.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,7 +1,6 @@
 puts "What is the hamster's name?"
 hamster_name=gets.chomp
 
-
 puts "What is the hamster's volume level (1-10)?"
 volume=gets.chomp.to_i
 until volume >= 1 && volume <= 10 
@@ -13,18 +12,33 @@ puts "What is the hamster's fur color?"
 fur_color=gets.chomp
 
 puts "Is the hamster a good candidate for adoption (y/n)?"
+
+
 candidate=gets.chomp
+
+
 until candidate.capitalize == "Y" || candidate.capitalize == "N"
 puts "Invalid input, please enter either 'y' or 'n'."
 candidate=gets.chomp
 end
 
-#if candidate.capitalize=="Y"
+if candidate.capitalize == "Y"
+	candidate = true
+else
+	candidate = false
+end
+
+if candidate
+	puts "Success!"
+end
 
 puts "What is the hamster's estimated age?"
-age=gets.chomp.to_f
-p age
+age=gets.chomp
+if age.length == 0
+    age = nil
+else 
+    age = age.to_f
+end
 
-p nil
 
 
