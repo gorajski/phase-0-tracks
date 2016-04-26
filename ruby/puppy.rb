542b01a30d2c99e5f40503ac05c08009f501071a
Nick Gorajski <nickgorajski@gmail.com>
2016-04-25 19:11:01 -0500

    Adds pseudocode to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+SPECIES ------------------------
+Cliffordicus puppicus
+
+CHARACTERISTICS ----------------
+Coat length: varies
+Coat color: red
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Runs
+Barks
+Rolls over
+Fetch
