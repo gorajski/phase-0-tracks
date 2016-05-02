5b48f6d3979229697b498f761d74a29fa67df9c8
Nick Gorajski <nickgorajski@gmail.com>
2016-05-02 09:34:30 -0500

    Rewrites shout.rb to use a mixin method.  Adds module and driver code to flight.rb.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,50 @@
+#  - - Define Modules - -  #
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
-  	words + " Yippeeeeeee!" + " :)"
+  def yell_happily(words)
+  	words + " Hey Eh Yeh Eh!" + " :)"
   end
 end
 
-puts Shout.yell_angrily("Grrrrrr!")
-puts Shout.yell_happily("Eureka!")
+#  - - Define Classes - -  #
+class Seventies_bands
+	include Shout
+end
+
+class Eighties_bands
+	include Shout
+end
+
+#  - - Main Method - -  #
+def main
+	tears_for_fears = Eighties_bands.new
+	otis_day_and_the_knights = Seventies_bands.new
+
+	puts "Tears for Fears says:"
+	puts tears_for_fears.yell_angrily("C'mon - I'm talking to you")
+	puts "Otis Day and the Knights say:"
+	puts otis_day_and_the_knights.yell_happily("A little bit louder now")
+
+end
+
+#  - - Execute Program - -  #
+main
+
+
+
+#  - - Old Code - -  #
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#   	words + " Yippeeeeeee!" + " :)"
+#   end
+# end
+
+# puts Shout.yell_angrily("Grrrrrr!")
+# puts Shout.yell_happily("Eureka!")
