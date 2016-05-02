cbd82c01a47b37cc3abc03f4b57d453e10b06f1d
Nick Gorajski <nickgorajski@gmail.com>
2016-05-01 21:55:48 -0500

    Adds getters and setters to santa.rb.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,11 @@
 class Santa
 
-	def initialize(gender,ethnicity,beard_length,beard_style)
+	def initialize(gender,ethnicity)
 		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
-		@beard = {length => beard_length, style => beard_style}
-		@laugh = "chortle"
-		@appetite = 10
 	end
 
 	def speak
@@ -19,31 +16,44 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking[8] = reindeer
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+
 end	
 
-santas = []
 
-puppies.each do |puppy|
-  puppy.bark
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "chimera", "asexual", "transgender male", "gender fluid", "dancer", "castrato"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Gorgon", "white", "mulatto", "Scottish-Canadian", "Arabian", "Italian"]
+example_genders.length.times do |i|
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+for j in 0...santas.length
+	santas[j].celebrate_birthday if j == 3
+	santas[j].eat_milk_and_cookies("oatmeal raisin") if j == 5
+	puts "Santa No. #{j} is #{santas[j].age} years old and of #{santas[j].ethnicity} background."
 end
 
 
-
-
-
-
-
-
-
-# Bad_Santa = Santa.new("male","Easter Islander")
-# Bad_Santa.speak
-# Bad_Santa.eat_milk_and_cookies("Thin mint")
-
-# santas = []
-# santas << Santa.new("agender", "black")
-# santas << Santa.new("female", "Latino")
-# santas << Santa.new("bigender", "white")
-# santas << Santa.new("male", "Japanese")
-# santas << Santa.new("female", "prefer not to say")
-# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-# santas << Santa.new("N/A", "N/A")
+santas[1].get_mad_at("Vixen")
+santas[6].gender = "castrato"
