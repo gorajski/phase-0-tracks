43728420defbd454206d10692832636e837f1ba2
Nick Gorajski <nickgorajski@gmail.com>
2016-05-20 11:12:46 -0500

    Creates simple website and adds code to modify image based on left and right clicks.

--- web_dev/client_side_javascript/stretch.js
+++ web_dev/client_side_javascript/stretch.js
@@ -1,15 +1,29 @@
 console.log("The script is running!");
 
 function stretchHeight() {
-	var photo = document.getElementById("hplogo");
-	var new_size = parseInt(photo.style.height) + 10;
-	if (isNaN(parseInt(photo.style.height))) {
-		photo.style.height = "102px";
-	} else {
-	photo.style.height = new_size + "px";		
-	}
+	var photo = document.getElementsByTagName("img")[0];
+	var new_size;
 
+	if (isNaN(parseInt(photo.style.height))) {
+	photo.style.height = "67px";
+	} else {
+		new_size = parseInt(photo.style.height) + 10;	//
+		photo.style.height = new_size + "px";
+	}
 }
 
-var photo = document.getElementById("hplogo");
+function shrinkHeight() {
+	var photo = document.getElementsByTagName("img")[0];
+	var new_size;
+
+	if (isNaN(parseInt(photo.style.height))) {
+	photo.style.height = "67px";
+	} else {
+		new_size = parseInt(photo.style.height) - 10;	//
+		photo.style.height = new_size + "px";
+	}
+}
+	
+var photo = document.getElementsByTagName("img")[0];
 photo.addEventListener("click", stretchHeight);
+photo.addEventListener("dblclick", shrinkHeight);
